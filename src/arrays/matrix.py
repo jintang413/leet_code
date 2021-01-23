479cb3337a2bc0fa7c1800364cc152fb9c65898c
Jin Tang <30931294+jintang413@users.noreply.github.com>
2021-01-23 09:04:02 -0500

    Update matrix.py

--- src/arrays/matrix.py
+++ src/arrays/matrix.py
@@ -29,3 +29,100 @@ def spiralOrder(matrix: List[List[int]]) -> List[int]:
         i += dr
         j += dc
     return ans
+
+
+def rotate(matrix):
+    length = len(matrix)
+
+    for i in range(length // 2):  # layer
+        k = length - i - 1
+        for r in range(i, length - i - 1):  # offset in layer
+            j = length - r - 1
+            matrix[r][i], matrix[k][r] = matrix[k][r], matrix[r][i]
+            matrix[k][r], matrix[j][k] = matrix[j][k], matrix[k][r]
+            matrix[j][k], matrix[i][j] = matrix[i][j], matrix[j][k]
+    print(matrix)
+
+
+def rotate_90(matrix):
+    print("rotate_90")
+    print_matrix(matrix)
+
+    # transpose
+    for r in range(len(matrix)):
+        for c in range(r):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+    print_matrix(matrix)
+
+    # flip it
+    for row in matrix:
+        row.reverse()
+
+    print_matrix(matrix)
+
+
+def rotate_270(matrix):  # -90
+    print("rotate_270")
+    print_matrix(matrix)
+
+    # flip it
+    for row in matrix:
+        row.reverse()
+
+    print_matrix(matrix)
+
+    # transpose
+    for r in range(len(matrix)):
+        for c in range(r):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+    print_matrix(matrix)
+
+
+def rotate_180(matrix):
+    print("rotate_180")
+    top = 0
+    bottom = len(matrix) - 1
+
+    print_matrix(matrix)
+
+    while top < bottom:
+        matrix[top], matrix[bottom] = matrix[bottom], matrix[top]
+        top += 1
+        bottom -= 1
+
+    # flip it
+    for row in matrix:
+        row.reverse()
+
+    print_matrix(matrix)
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+    print("-----------")
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    rotate_90(matrix)
+
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    rotate_270(matrix)
+
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    rotate_180(matrix)
